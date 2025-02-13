8ce3c57c397a910f543eec566df2469c5869245b
Nyle Coelho <biancacoelhoo05@gmail.com>
2025-02-12 20:22:06 -0400

    Refactor adicionarAmigo function to accept input parameter and handle null values; update input retrieval in app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-let amigo = document.getElementById('amigo').value;
 
-function adicionarAmigo() {
-    let listaAmigos = []
-    listaAmigos.push(`${amigo}`);
+let listaAmigos = []
+
+function adicionarAmigo(amigos) {
+
+    amigos = document.getElementById('amigo').value;
+
+if (amigos != null){
+    listaAmigos.push(amigos);
     console.log(listaAmigos);
 }
+else {
+    alert("It cant be null");
+}}
 
 function sortearAmigo() {
 
